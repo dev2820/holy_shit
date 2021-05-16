e770ac98ff6aa1c33d3ad4f1691b4e8a2ae923ee
spaff13 <spaff316@gmail.com>
2021-05-16 19:56:50 +0900

    express

--- dongwon/myapp/app.js
+++ dongwon/myapp/app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const session = require('express-session')
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -16,8 +18,17 @@ app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser('1234'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  resave: false,
+  saveUninitialized: false,
+  secret: '1234',
+  cookie:{
+    httpOnly: true,
+    secure: false,
+  }
+}))
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- dongwon/myapp/routes/index.js
+++ dongwon/myapp/routes/index.js
@@ -1,9 +1,44 @@
 var express = require('express');
+const app = express();
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.route('/').get(function(req, res){
+  console.log('no 새숀')
+  if(req.session.user)
+    res.redirect('/home')
+  else
+    res.redirect('/login')
+})
+
+router.route('/login').get(function(req,res){
+  if(req.session.user)
+    res.redirect('/home');
+  else{
+    res.sendFile(__dirname+'/image/login.html')
+   }
+})
+
+router.route('/logout').get(function(req,res){
+  req.session.destroy(function(){
+    req.session;
+  })
+  res.redirect('/login');
+})
+
+
+router.post('/make_session',(req, res)=>{
+  req.session.user = {
+    "name" : "yoo",
+    "id" : "won",
+    "pw" : "1234"
+  }
+  res.redirect('/home')
+})
+
+router.route('/home').get((req, res) =>{
+  res.send("home")
+})
+
+
 
 module.exports = router;
