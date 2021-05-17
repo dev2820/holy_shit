3eace198e4640cf90c1d592b9bea0c0d8dced927
dev2820 <second28200@gmail.com>
2021-05-17 20:44:45 +0900

    현호가 짠 코드 다듬어서 login 페이지가 제대로 나오도록 수정 (이미지, login.html,css파일들 public 위치로 옮김,이렇게 해야 image와 css파일 요청이 제대로 작동함)

--- dongwon/myapp/routes/index.js
+++ dongwon/myapp/routes/index.js
@@ -1,5 +1,5 @@
 var express = require('express');
-const app = express();
+const path = require('path');
 var router = express.Router();
 
 router.route('/').get(function(req, res){
@@ -14,8 +14,27 @@ router.route('/login').get(function(req,res){
   if(req.session.user)
     res.redirect('/home');
   else{
-    res.sendFile(__dirname+'/image/login.html')
-   }
+    res.sendFile(path.join(__dirname,'../public','login.html'));
+  }
+})
+
+router.route('/signup').get(function(req,res){/*signup 페이지를 호스팅하는 루틴 */
+  if(req.session.user)/*로그인 된 유저는 signup할 수 없도록 home으로 redirect한다. */
+    res.redirect('/home');
+  else{/*public내부의 signup.html페이지를 호스팅 */
+    res.sendFile(path.join(__dirname,'../public','signup.html'));
+  }
+})
+
+router.route('/signup').post(function(req,res){
+  /* signup 로직 구현부 
+   * /signup 경로로 post 요청이 들어오면 수행된다.
+   * 데이터 베이스와 연동할 예정 (mariaDB)
+   * 현재는 post로 보내진 name과 pass를 console에 띄우는 기능만 한다.
+  */
+  const name = req.body.userName || null;
+  const pass = req.body.userPass || null;
+  console.log(name,pass);
 })
 
 router.route('/logout').get(function(req,res){
